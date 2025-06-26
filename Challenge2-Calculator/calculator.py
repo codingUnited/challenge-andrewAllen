1d320a448336e7d78085b97fd47b0722c3c0e83e
Andrew Allen <10346323+PeacefulDivide@users.noreply.github.com>
2025-06-26 01:02:45 -0500

    Create calculator.py

---
+++ Challenge2-Calculator/calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to your calculator!")
+
+num1 = input("Enter the first number: ")
+operator = input("Enter an operation (+, -, *, /): ")
+num2 = input("Enter the second number: ")
+
+# TODO: Convert numbers and perform the operation
